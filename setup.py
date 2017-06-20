ca717897fea1d7efac2071aa558eaf0796509b59
Shoya Matsumori <smatsumori@Fomalhaut.local>
2017-06-20 17:47:08 +0900

    MOD: updating for pip

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from src import __author__, __version__, __license__
+
+setup(
+    name='pip_github_test',
+    version=__version__,
+    description='easy uploader for google drive',
+    license=__license__,
+    author=__author__,
+    author_email='smatsumori[at]ailab.ics.keio.ac.jp',
+    url='https://github.com/smatsumori/pDrive.git',
+    keywords='python googledrive',
+    packages=find_packages(),
+    install_requires=['google-api-python-client'],
+)
